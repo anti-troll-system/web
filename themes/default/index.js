b83083580fbcc6a517fdfba98ee0ac552fa2e323
Johnny Seyd <johnnyseyd@gmail.com>
2017-06-12 09:11:37 +0200

    * new style + temp content

--- themes/default/index.js
+++ themes/default/index.js
@@ -1,8 +1,8 @@
 exports.install = function() {
 
 	// Merging static files
-	F.merge('/default/js/default.js', '/js/jctajr.min.js', '=default/public/js/default.js', '=default/public/js/ui.js');
-	F.merge('/default/css/default.css', '/css/bootstrap.min.css', '=default/public/css/default.css', '=default/public/css/ui.css');
+	F.merge('/default/js/default.js', '/js/jctajr.min.js', '/js/JS.Responsive.custom.min.js', '=default/public/js/default.js', '=default/public/js/ui.js');
+	F.merge('/default/css/default.css', '/css/bootstrap.min.css', '=default/public/css/default.css', '=default/public/css/ui.css', '=default/public/css/style.css');
 
 	// Localization & Minification
 	F.localize('/default/components/*.html', ['compress']);
--- themes/default/public/js/default.js
+++ themes/default/public/js/default.js
@@ -1,5 +1,15 @@
 var common = {};
 
+JS.Responsive
+       .addHorizontalBreakPoint( 'x-small',  320 )
+       .addHorizontalBreakPoint( 'small',    480 )
+	   .addHorizontalBreakPoint( 'smadium',  767 )
+       .addHorizontalBreakPoint( 'medium',   992 )
+       .addHorizontalBreakPoint( 'large',   1280 )
+       .addHorizontalBreakPoint( 'x-large', 1920 );
+	   
+JS.Responsive.init();
+
 SETTER(true, 'loading', 'hide', 1000);
 
 ON('ready', function() {
@@ -7,13 +17,28 @@ ON('ready', function() {
 	$('.mainmenu').find('a[href="{0}"]'.format(NAVIGATION.url)).addClass('selected');
 	setTimeout(function() {
 		$('body').addClass('animate');
+		onresize()
 	}, 100);
 });
 
 $(window).on('resize', function() {
 	EMIT('resize', $(window));
+	onresize();
 });
 
 function mainmenu() {
 	SET('common.mainmenu', common.mainmenu ? false : true);
+}
+
+
+// -------------------------
+
+function onresize() {
+	
+	// we want to have content with minimum height
+	// that footer is always at the bottom of the page
+	var windowHeight = $(window).height(),
+		footerHeight = $('footer')[0].offsetHeight,
+		contentHeight = windowHeight - footerHeight;
+	$('.content').css('min-height', contentHeight+'px');
 }
